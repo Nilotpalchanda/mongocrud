b8307a79f831e30618b9403aabef360f18071ea9
Nilotpalchanda <nilotpalchanda14@gmail.com>
2018-02-25 02:05:33 +0530

    trying to image save in mongodb

--- server.js
+++ server.js
@@ -1,14 +1,45 @@
 //call module
 var express = require('express');
 var app = express();
+var path = require('path')
+var http = require('http');
+var multer = require('multer')
+var mongoose = require('mongoose');
+//var upload = multer({dest: './uploads'})
 var routes = require('./routes/index')
-const bodyParser= require('body-parser')
+var bodyParser = require('body-parser');
 //var port = process.env.PORT|| 3000;
-app.use(bodyParser.urlencoded({extended: true}))
+
+// Multer path define
+// var storage = multer.diskStorage({
+// 	destination: './uploads',
+// 	filename: function(req,file,cb){
+// 		cb(null,file.fieldname + '_' + Date.now() + path.extname(file.originalname) );
+// 	}
+// });
+
+var storage = multer.diskStorage({
+	destination: (req,file,cb)=>{
+		cb(null,'./uploads')
+	},
+	filename: (req,file,cb)=>{
+		cb(null,file.originalname);
+	}
+})
+// init upload
+
+var upload = multer({
+	storage: storage
+});
+
+
 
 app.set('view engine','ejs');
-
+app.use(bodyParser.urlencoded({ extended : true }));
+app.use(bodyParser.json());
 //connect Mongodb
+
+
 const MongoClient = require('mongodb').MongoClient
 var db
 MongoClient.connect('mongodb://admin:test@ds147118.mlab.com:47118/neelcrud', (err, client) => {
@@ -19,18 +50,23 @@ MongoClient.connect('mongodb://admin:test@ds147118.mlab.com:47118/neelcrud', (er
   })
 })
 
+
 //routes
 app.get('/', (req,res)=>{
 	res.render('index')
 })
-app.post('/posts', (req,res)=>{
+app.post('/posts',  upload.single('post_iamge'), (req,res)=>{
+	
 	  db.collection('posts').save(req.body, (err, result) => {
     if (err) return console.log(err)
 
     console.log('saved to database')
 	console.log(req.body)
-    res.render('index.ejs',)
+	console.log(req.file)
+	//console.log(req.file.originalname)
+    res.render('index.ejs')
   })
+	  
 })
 
 app.get('/blog',(req,res)=>{
